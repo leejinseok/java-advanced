5dc8c2359a4c8d1a674e66c5f2a5a740a1f29531
leejinseok <sonaky47@gmail.com>
2019-11-15 14:16:08 +0900

    chap04 created

---
+++ chap03/src/main/java/AnonymousClass.java
@@ -0,0 +1,14 @@
+public class AnonymousClass {
+  private static int count = 0;
+
+//  public static void main( String[] args ) {
+//    new Thread(
+//      new Runnable() {
+//        @Override
+//        public void run() {
+//        }
+//      }
+//    ).start();
+//  }
+
+}
---
+++ chap03/src/main/java/SimpleAbstractClass.java
@@ -0,0 +1,8 @@
+
+public abstract class SimpleAbstractClass {
+  public void performAction() {
+
+  }
+
+  public abstract void performAnotherAction();
+}
